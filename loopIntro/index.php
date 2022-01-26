94544711305f04780d6112b3bbaeee56d65a9393
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-01-25 18:08:25 -0600

    PHP looping intro

---
+++ loopIntro/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+// $counter = 0;
+
+// while ($counter <= 5) {
+//     echo $counter . "<br>";
+//     $counter++;
+//     $message = "null";
+// }
+
+// for ($counter = 10; $counter > 0; $counter--) {
+//     $message = $message . $counter . "<br>";
+// }
+// echo $message;
+
+
+$arr = array("Robert", "Jimmy", "JPJ", "Bonham");
+// while ($counter < count($arr)) {
+//     echo $arr[$counter] . "<br>";
+//     $counter++;
+// }
+
+
+for ($counter = 0; $counter < count($arr); $counter++) {
+    echo $arr[$counter] . "<br>";
+}
+
+?>
